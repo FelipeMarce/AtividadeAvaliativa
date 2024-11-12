00d413ebb7d48a662a08d5720e985c7784f4c5cf
FelipeMarce <0000965391@senaimgaluno.com.br>
2024-11-12 16:25:26 -0300

    Exe_01 e Exe_02

--- AtividadeAvaliativa/src/Ex1.java
+++ AtividadeAvaliativa/src/Ex1.java
@@ -4,10 +4,45 @@ a. a maior e a menor altura do grupo;
 b. média de altura dos homens;
 c. o número de mulheres.
  */
+
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		double[] altura = new double[10];
+		double maior = 0, menor = 999999999, total = 0;
+		int m = 0, h = 0;
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Digite a sua altura: ");
+			altura[i] = sc.nextDouble();
+			System.out.print("Insira seu sexo (H homem, M mulher): ");
+			char sexo = sc.next().charAt(0);
+
+			if (altura[i] <= menor) {
+				menor = altura[i];
+			}
+			if (altura[i] >= maior) {
+				maior = altura[i];
+			}
+			if (sexo == 'H' || sexo == 'h') {
+				total = altura[i] + total;
+				h++;
+			}
+			if (sexo == 'M' || sexo == 'm') {
+				m++;
+			}
+		}
+
+		System.out.println("A maior altura é: " + maior + "\nA menor altura é: " + menor);
+		System.out.printf("Média da altura dos homens %.2f: ", total / h);
+		System.out.print("\nQuantidade de mulheres: " + m);
+
+		sc.close();
 
 	}
 
--- AtividadeAvaliativa/src/Ex2.java
+++ AtividadeAvaliativa/src/Ex2.java
@@ -1,10 +1,30 @@
 /*Criar um programa que calcule a média de salários de uma empresa, 
  * pedindo ao usuário a grade de funcionários e os salários, e devolvendo a média salarial.
  */
+
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double total = 0;
+		
+		System.out.print("Digite o número de funcionarios: ");
+		int funcionarios = sc.nextInt();
+		
+		for(int i = 1; i <= funcionarios; i++) {
+			System.out.print("Digite o salário do " + i + "° funcionario: ");
+			double salario = sc.nextDouble();
+			
+			total = salario + total;
+		}
+		
+		System.out.printf("Média dos salários dos funcionarios %.2f: ", total/funcionarios);
+		
+		sc.close();
 
 	}
 
